d3172858250a8a2a4691b9bfc0ada88fe034d350
Prachi Derle <147366892+prachiderle@users.noreply.github.com>
2025-11-22 15:15:52 +0530

    Add files via upload

---
+++ Assignments/Assignment6/Program2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int FindMax(int a,int b)
+{
+    if(a>b)
+      return a;
+    else 
+    return b;
+
+}
+
+int main()
+{
+    int num1,num2,result;
+    printf("enter two numbers:");
+    scanf("%d%d",&num1,&num2);
+
+    result=FindMax(num1,num2);
+    printf("Maximum is:%d\n",result);
+
+    return 0;
+
+}
---
+++ Assignments/Assignment6/Program4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void CheckNumberType(int num)
+{
+    if (num > 0)
+        printf("%d is Positive\n", num);
+    else if (num < 0)
+        printf("%d is Negative\n", num);
+    else
+        printf("Number is Zero\n");
+}
+
+int main()
+{
+    int number;
+    printf("Enter number: ");
+    scanf("%d", &number);
+    CheckNumberType(number);
+    return 0;
+}
