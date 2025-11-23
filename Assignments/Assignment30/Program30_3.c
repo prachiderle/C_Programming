1272a58d585cf7a6c2cbbbeffe743b61358fd142
Prachi Derle <147366892+prachiderle@users.noreply.github.com>
2025-11-23 22:40:15 +0530

    Add files via upload

---
+++ Assignments/Assignment30/Program30_3.c
@@ -0,0 +1,50 @@
+/////////////////////////////////////////////////////////////////////////////////////
+// 
+//  Function Name : Pattern Printing
+//  Description   : It Display the As per Input Pattern
+//  Input         : iRow 5 , iCol 5 
+//  Output        : $   *   *   *   *
+//                  *   $   *   *   *
+//                  *   *   $   *   *
+//                  *   *   *   $   *
+//                  *   *   *   *   $                
+//  Author        : Prachi Bhausaheb Derle
+//  Date          : 23/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void Pattern( int iRow, int iCol)
+{
+    int i = 0 , j= 0;
+
+    for(i = 1; i<=iRow; i++)
+    {
+        for(j = 1; j <= iCol ;j++)
+        {
+            if(i == j)
+            {
+                printf("$\t");
+            }
+            else
+            {
+                printf("*\t");
+            }
+        }
+        j++;
+        printf("\n");
+    }
+    
+}
+int main()
+{
+    int iValue1 =0,iValue2 = 0;
+
+    printf("Enter the Row and Colunm : ");
+    scanf("%d%d",&iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
